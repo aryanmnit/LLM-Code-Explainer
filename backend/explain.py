55191dbfcefb357e9bec2f8afab50596aead5169
aryanmnit <aryanvermaofficial280@gmail.com>
2025-07-05 16:45:18 +0530

    Update explain.py

--- backend/explain.py
+++ backend/explain.py
@@ -1,19 +1,20 @@
-import openai
+from openai import OpenAI
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_base = "https://api.a4f.co/v1"
-
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+    base_url=os.getenv("OPENAI_BASE_URL")
+)
 
 def generate_explanation(code, language):
     prompt = f"Explain this {language} code in a beginner-friendly way, line by line:\n\n{code}"
-    response = openai.ChatCompletion.create(
+    response = client.chat.completions.create(
         model="provider-2/gpt-3.5-turbo",
         messages=[{"role": "user", "content": prompt}],
         temperature=0.4,
         max_tokens=1000
     )
-    return response.choices[0].message["content"]
+    return response.choices[0].message.content
