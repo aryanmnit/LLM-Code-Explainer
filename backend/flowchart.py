d8cd739a98c51357f92d84b10445a43451e13623
aryanmnit <aryanvermaofficial280@gmail.com>
2025-07-05 16:46:03 +0530

    Update flowchart.py

--- backend/flowchart.py
+++ backend/flowchart.py
@@ -1,16 +1,18 @@
-import openai
+from openai import OpenAI
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_base = "https://api.a4f.co/v1"
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+    base_url=os.getenv("OPENAI_BASE_URL")
+)
 
 
 def generate_flowchart(code, language):
-    prompt = f"Generate a Mermaid.js flowchart for the following {language} code:\n\n{code}"
-    response = openai.ChatCompletion.create(
+    prompt = f"Generate a Mermaid.js flowchart for the following {language} code and reply only the flowchart:\n\n{code}"
+    response = client.chat.completions.create(
         model="provider-2/gpt-3.5-turbo",
         messages=[{"role": "user", "content": prompt}],
         temperature=0.5,
